50f50740fc87c1880498a141c579a415ef6eb40b
Wemerson Jesus <wejesuss@gmail.com>
2020-06-24 17:25:11 -0300

    Fix white spaces coming from req.body

--- app/controllers/maker.js
+++ app/controllers/maker.js
@@ -1,10 +1,20 @@
-const fs = require('fs')
-const path = require("path")
 const createReadme = require('../utils/createReadme')
 let readme = null
+
 module.exports = {
     post (req, res) {
-        readme = createReadme.init(req.body)
+        let features = req.body.features
+        let images = req.body.images
+
+        features = features.filter(feature => feature)
+        images = images.filter(image => image)
+
+        readme = createReadme.init({
+            ...req.body,
+            features,
+            images
+        })
+        
         return res.redirect("/ready")
     },
     ready (req, res) {
