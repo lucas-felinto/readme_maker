4cf17b824b47cfa22b913b78977f9869a6ffef40
Wemerson Jesus <wejesuss@gmail.com>
2020-06-24 18:08:51 -0300

    Feature/delete input (#11)
    * Add favicon
    
    * Refactor: readme creator base
    
    * Delete data.json
    
    * Delete data.json and add readme Model
    
    * Fix white spaces coming from req.body
    
    * Funcionality - remove url field
    
    * Limit of resizing the textarea, limit the width of url's input, adjust margin of the 'about' box
    
    * Remove spaces and change the text content of the option element

--- public/scripts.js
+++ public/scripts.js
@@ -2,6 +2,62 @@ const initialBox = document.querySelector('.initial-box')
 const logoTitle = document.querySelector('.logo-title')
 const imagesGif = document.querySelector('.images-gif')
 const about = document.querySelector('.about')
+const lastInformations = document.querySelector('.last-informations')
+
+function removeField(event) {
+    const divUrl = event.target.parentNode
+
+    divUrl.remove()
+}
+
+function createRemoveButton(className, textContent) {
+    const pRemoveButton = document.createElement("p")
+    pRemoveButton.classList.add(className)
+    pRemoveButton.innerHTML = textContent
+    pRemoveButton.onclick = removeField
+
+    return pRemoveButton
+}
+
+document.querySelector('#add-image').addEventListener("click", event => {
+    const divInput = document.querySelector('#url-img')
+    const inputUrl = document.querySelectorAll('.input-url')
+
+    const newField = inputUrl[inputUrl.length - 1].cloneNode(true)
+
+    if (newField.children[0].value == "") return false
+
+    if(inputUrl.length < 2) {
+        const pRemoveImage = createRemoveButton("remove-image", "- Remover Arquivo")
+
+        newField.appendChild(pRemoveImage)
+    } else {
+        newField.children[1].onclick = removeField
+    }
+
+    newField.children[0].value = ""
+    divInput.appendChild(newField)
+})
+
+document.querySelector('#add-tec').addEventListener("click", event => {
+    const divInput = document.querySelector('#features')
+    const inputFeature = document.querySelectorAll('.features-input')
+
+    const newField = inputFeature[inputFeature.length - 1].cloneNode(true)
+
+    if (newField.children[0].value == "") return false
+
+    if(inputFeature.length < 2) {
+        const pRemoveTecnology = createRemoveButton("remove-tec", "- Remover Tecnologia")
+
+        newField.appendChild(pRemoveTecnology)
+    } else {
+        newField.children[1].onclick = removeField
+    }
+
+    newField.children[0].value = ""
+    divInput.appendChild(newField)
+})
 
 document.querySelector('#button-start').addEventListener("click", function() {
     initialBox.classList.add('hidden')
@@ -18,18 +74,6 @@ document.querySelector('#button-next').addEventListener("click", () => {
     }, 200)
 })
 
-document.querySelector('#add-image').addEventListener("click", addUrlInput => {
-    const divInput = document.querySelector('#url-img')
-    const inputUrl = document.querySelector('.input-url')
-
-    const newField = inputUrl.cloneNode(true)
-
-    if (newField.children[0].value == "") return false
-
-    newField.children[0].value = ""
-    divInput.appendChild(newField)
-})
-
 document.querySelector('#button-next2').addEventListener("click", () => {
     event.preventDefault()
     imagesGif.classList.remove('show')
@@ -42,18 +86,6 @@ document.querySelector('#button-next3').addEventListener("click", () => {
     event.preventDefault()
     about.classList.remove('show')
     setTimeout(() => {
-        document.querySelector('.last-informations').classList.add('show')
+        lastInformations.classList.add('show')
     }, 200)
-})
-
-document.querySelector('#add-tec').addEventListener("click", addUrlInput => {
-    const divInput = document.querySelector('#features')
-    const inputUrl = document.querySelector('.features-input')
-
-    const newField = inputUrl.cloneNode(true)
-
-    if (newField.children[0].value == "") return false
-
-    newField.children[0].value = ""
-    divInput.appendChild(newField)
 })
