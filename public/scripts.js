bb2c5a7b8949dd94437f1ef2db3c6e9dc2978672
Wemerson Jesus <wejesuss@gmail.com>
2020-06-25 18:47:40 -0300

    Fix devtools console error because of not found elements

--- public/scripts.js
+++ public/scripts.js
@@ -19,73 +19,85 @@ function createRemoveButton(className, textContent) {
     return pRemoveButton
 }
 
-document.querySelector('#add-image').addEventListener("click", event => {
-    const divInput = document.querySelector('#url-img')
-    const inputUrl = document.querySelectorAll('.input-url')
+const addImage = document.querySelector('#add-image')
+const addTec = document.querySelector('#add-tec')
 
-    const newField = inputUrl[inputUrl.length - 1].cloneNode(true)
+if(addImage && addTec) {
+    addImage.addEventListener("click", event => {
+        const divInput = document.querySelector('#url-img')
+        const inputUrl = document.querySelectorAll('.input-url')
+    
+        const newField = inputUrl[inputUrl.length - 1].cloneNode(true)
+    
+        if (newField.children[0].value == "") return false
+    
+        if(inputUrl.length < 2) {
+            const pRemoveImage = createRemoveButton("remove-image", "- Remover Arquivo")
+    
+            newField.appendChild(pRemoveImage)
+        } else {
+            newField.children[1].onclick = removeField
+        }
+    
+        newField.children[0].value = ""
+        divInput.appendChild(newField)
+    })
+    
+    addTec.addEventListener("click", event => {
+        const divInput = document.querySelector('#features')
+        const inputFeature = document.querySelectorAll('.features-input')
+    
+        const newField = inputFeature[inputFeature.length - 1].cloneNode(true)
+    
+        if (newField.children[0].value == "") return false
+    
+        if(inputFeature.length < 2) {
+            const pRemoveTecnology = createRemoveButton("remove-tec", "- Remover Tecnologia")
+    
+            newField.appendChild(pRemoveTecnology)
+        } else {
+            newField.children[1].onclick = removeField
+        }
+    
+        newField.children[0].value = ""
+        divInput.appendChild(newField)
+    })
+}
 
-    if (newField.children[0].value == "") return false
+const buttonStart = document.querySelector('#button-start')
+const buttonNext = document.querySelector('#button-next')
+const buttonNext2 = document.querySelector('#button-next2')
+const buttonNext3 = document.querySelector('#button-next3')
 
-    if(inputUrl.length < 2) {
-        const pRemoveImage = createRemoveButton("remove-image", "- Remover Arquivo")
-
-        newField.appendChild(pRemoveImage)
-    } else {
-        newField.children[1].onclick = removeField
-    }
-
-    newField.children[0].value = ""
-    divInput.appendChild(newField)
-})
-
-document.querySelector('#add-tec').addEventListener("click", event => {
-    const divInput = document.querySelector('#features')
-    const inputFeature = document.querySelectorAll('.features-input')
-
-    const newField = inputFeature[inputFeature.length - 1].cloneNode(true)
-
-    if (newField.children[0].value == "") return false
-
-    if(inputFeature.length < 2) {
-        const pRemoveTecnology = createRemoveButton("remove-tec", "- Remover Tecnologia")
-
-        newField.appendChild(pRemoveTecnology)
-    } else {
-        newField.children[1].onclick = removeField
-    }
-
-    newField.children[0].value = ""
-    divInput.appendChild(newField)
-})
-
-document.querySelector('#button-start').addEventListener("click", function() {
-    initialBox.classList.add('hidden')
-    setTimeout(function() {
-        logoTitle.classList.add('show')
-    }, 200)
-})
-
-document.querySelector('#button-next').addEventListener("click", () => {
-    event.preventDefault()
-    logoTitle.classList.remove('show')
-    setTimeout(() => {
-        imagesGif.classList.add('show')
-    }, 200)
-})
-
-document.querySelector('#button-next2').addEventListener("click", () => {
-    event.preventDefault()
-    imagesGif.classList.remove('show')
-    setTimeout(() => {
-        about.classList.add('show')
-    }, 200)
-})
-
-document.querySelector('#button-next3').addEventListener("click", () => {
-    event.preventDefault()
-    about.classList.remove('show')
-    setTimeout(() => {
-        lastInformations.classList.add('show')
-    }, 200)
-})
+if(buttonStart && buttonNext && buttonNext2 && buttonNext3) {
+    buttonStart.addEventListener("click", function() {
+        initialBox.classList.add('hidden')
+        setTimeout(function() {
+            logoTitle.classList.add('show')
+        }, 200)
+    })
+    
+    buttonNext.addEventListener("click", () => {
+        event.preventDefault()
+        logoTitle.classList.remove('show')
+        setTimeout(() => {
+            imagesGif.classList.add('show')
+        }, 200)
+    })
+    
+    buttonNext2.addEventListener("click", () => {
+        event.preventDefault()
+        imagesGif.classList.remove('show')
+        setTimeout(() => {
+            about.classList.add('show')
+        }, 200)
+    })
+    
+    buttonNext3.addEventListener("click", () => {
+        event.preventDefault()
+        about.classList.remove('show')
+        setTimeout(() => {
+            lastInformations.classList.add('show')
+        }, 200)
+    })
+}
