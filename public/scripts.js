70dd859911c7921c1968ecd9a85f6ae95416bcff
Wemerson Jesus <wejesuss@gmail.com>
2020-06-26 08:14:56 -0300

    Create go back functionality

--- public/scripts.js
+++ public/scripts.js
@@ -3,6 +3,7 @@ const logoTitle = document.querySelector('.logo-title')
 const imagesGif = document.querySelector('.images-gif')
 const about = document.querySelector('.about')
 const lastInformations = document.querySelector('.last-informations')
+const backButtons = document.querySelectorAll(".label > img")
 
 function removeField(event) {
     const divUrl = event.target.parentNode
@@ -19,73 +20,102 @@ function createRemoveButton(className, textContent) {
     return pRemoveButton
 }
 
-document.querySelector('#add-image').addEventListener("click", event => {
-    const divInput = document.querySelector('#url-img')
-    const inputUrl = document.querySelectorAll('.input-url')
+const addImage = document.querySelector('#add-image')
+const addTec = document.querySelector('#add-tec')
 
-    const newField = inputUrl[inputUrl.length - 1].cloneNode(true)
+if(addImage && addTec) {
+    addImage.addEventListener("click", event => {
+        const divInput = document.querySelector('#url-img')
+        const inputUrl = document.querySelectorAll('.input-url')
 
-    if (newField.children[0].value == "") return false
+        const newField = inputUrl[inputUrl.length - 1].cloneNode(true)
 
-    if(inputUrl.length < 2) {
-        const pRemoveImage = createRemoveButton("remove-image", "- Remover Arquivo")
+        if (newField.children[0].value == "") return false
 
-        newField.appendChild(pRemoveImage)
-    } else {
-        newField.children[1].onclick = removeField
-    }
+        if(inputUrl.length < 2) {
+            const pRemoveImage = createRemoveButton("remove-image", "- Remover Arquivo")
 
-    newField.children[0].value = ""
-    divInput.appendChild(newField)
-})
+            newField.appendChild(pRemoveImage)
+        } else {
+            newField.children[1].onclick = removeField
+        }
 
-document.querySelector('#add-tec').addEventListener("click", event => {
-    const divInput = document.querySelector('#features')
-    const inputFeature = document.querySelectorAll('.features-input')
+        newField.children[0].value = ""
+        divInput.appendChild(newField)
+    })
 
-    const newField = inputFeature[inputFeature.length - 1].cloneNode(true)
+    addTec.addEventListener("click", event => {
+        const divInput = document.querySelector('#features')
+        const inputFeature = document.querySelectorAll('.features-input')
 
-    if (newField.children[0].value == "") return false
+        const newField = inputFeature[inputFeature.length - 1].cloneNode(true)
 
-    if(inputFeature.length < 2) {
-        const pRemoveTecnology = createRemoveButton("remove-tec", "- Remover Tecnologia")
+        if (newField.children[0].value == "") return false
 
-        newField.appendChild(pRemoveTecnology)
-    } else {
-        newField.children[1].onclick = removeField
-    }
+        if(inputFeature.length < 2) {
+            const pRemoveTecnology = createRemoveButton("remove-tec", "- Remover Tecnologia")
 
-    newField.children[0].value = ""
-    divInput.appendChild(newField)
-})
+            newField.appendChild(pRemoveTecnology)
+        } else {
+            newField.children[1].onclick = removeField
+        }
 
-document.querySelector('#button-start').addEventListener("click", function() {
-    initialBox.classList.add('hidden')
-    setTimeout(function() {
-        logoTitle.classList.add('show')
-    }, 200)
-})
+        newField.children[0].value = ""
+        divInput.appendChild(newField)
+    })
+}
 
-document.querySelector('#button-next').addEventListener("click", () => {
-    event.preventDefault()
-    logoTitle.classList.remove('show')
-    setTimeout(() => {
-        imagesGif.classList.add('show')
-    }, 200)
-})
+const buttonStart = document.querySelector('#button-start')
+const buttonNext = document.querySelector('#button-next')
+const buttonNext2 = document.querySelector('#button-next2')
+const buttonNext3 = document.querySelector('#button-next3')
 
-document.querySelector('#button-next2').addEventListener("click", () => {
-    event.preventDefault()
-    imagesGif.classList.remove('show')
-    setTimeout(() => {
-        about.classList.add('show')
-    }, 200)
-})
+if(buttonStart && buttonNext && buttonNext2 && buttonNext3) {
+    buttonStart.addEventListener("click", function() {
+        initialBox.classList.add('hidden')
+        setTimeout(function() {
+            logoTitle.classList.add('show')
+        }, 200)
+    })
 
-document.querySelector('#button-next3').addEventListener("click", () => {
-    event.preventDefault()
-    about.classList.remove('show')
-    setTimeout(() => {
-        lastInformations.classList.add('show')
-    }, 200)
-})
+    buttonNext.addEventListener("click", () => {
+        event.preventDefault()
+        logoTitle.classList.remove('show')
+        setTimeout(() => {
+            imagesGif.classList.add('show')
+        }, 200)
+    })
+
+    buttonNext2.addEventListener("click", () => {
+        event.preventDefault()
+        imagesGif.classList.remove('show')
+        setTimeout(() => {
+            about.classList.add('show')
+        }, 200)
+    })
+
+    buttonNext3.addEventListener("click", () => {
+        event.preventDefault()
+        about.classList.remove('show')
+        setTimeout(() => {
+            lastInformations.classList.add('show')
+        }, 200)
+    })
+}
+
+// backward button
+if(backButtons) {
+    backButtons.forEach(button => {
+        button.addEventListener("click", () => {
+            const isBeingShowed = button
+            .parentElement
+            .parentElement
+
+            isBeingShowed.classList.remove("show")
+
+            setTimeout(() => {
+                isBeingShowed.previousElementSibling.classList.add("show")
+            }, 200)
+        })
+    })
+}
