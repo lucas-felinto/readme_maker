cf66c12b00a3fd38d9df7c836da8ccd80d745bef
lucas-felinto <lucasfepinto@gmail.com>
2020-07-10 13:46:58 -0300

    add minimal requirements

--- app/utils/createReadme.js
+++ app/utils/createReadme.js
@@ -1,4 +1,5 @@
-const { url }= require("../config/website");
+/* eslint-disable quotes */
+const { url } = require("../config/website");
 
 const Readme = {
   init(data) {
@@ -12,6 +13,7 @@ const Readme = {
     this.getLinks();
     this.getImagesUrl();
     this.getImages();
+    this.getRequirements();
     this.getTecnologies();
     this.getContribution();
 
@@ -27,7 +29,7 @@ const Readme = {
     this.reason = this.data.reason;
   },
   getLogo() {
-    if(this.data.logo) {
+    if (this.data.logo) {
       this.logo = `<p align="center">\n  <img src="${this.data.logo}" width="300" heigth="300">\n</p>`;
     } else {
       this.logo = "";
@@ -35,48 +37,57 @@ const Readme = {
   },
   getLicense() {
     const licenses = {
-      MIT: "\n    <img alt=\"License\" src=\"https://img.shields.io/badge/license-MIT-%23F8952D\">",
-      APACHE: "\n    <img alt=\"License\" src=\"https://img.shields.io/badge/license-APACHE-%23F8952D\">",
-      GLP: "\n    <img alt=\"License\" src=\"https://img.shields.io/badge/license-GLP-%23F8952D\">"
+      MIT:
+        '\n    <img alt="License" src="https://img.shields.io/badge/license-MIT-%23F8952D">',
+      APACHE:
+        '\n    <img alt="License" src="https://img.shields.io/badge/license-APACHE-%23F8952D">',
+      GLP:
+        '\n    <img alt="License" src="https://img.shields.io/badge/license-GLP-%23F8952D">',
     };
 
     this.licenseCode = this.data.license;
-    this.license = 
-        `<p align="center">\n  <a href="LICENSE">${licenses[this.licenseCode]}\n  </a>\n</p>`;
+    this.license = `<p align="center">\n  <a href="LICENSE">${
+      licenses[this.licenseCode]
+    }\n  </a>\n</p>`;
   },
   getLinks() {
-    let links = "<a href=\"#information_source-sobre\">Sobre</a>&nbsp;|&nbsp;\n  <a href=\"#interrobang-motivo\">Motivo</a>&nbsp;|&nbsp;\n  <a href=\"#rocket-tecnologias-utilizadas\">Tecnologias</a>&nbsp;|&nbsp;";
+    let links =
+      '<a href="#information_source-sobre">Sobre</a>&nbsp;|&nbsp;\n  <a href="#interrobang-motivo">Motivo</a>&nbsp;|&nbsp;\n  <a href="#seedling-requisitos-mínimos">Requisitos</a>&nbsp;|&nbsp;\n  <a href="#rocket-tecnologias-utilizadas">Tecnologias</a>&nbsp;|&nbsp;';
     if (this.data.contribution == "sim") {
-      links += "\n  <a href=\"#link-como-contribuir\">Como Contribuir</a>&nbsp;|&nbsp;";
+      links +=
+        '\n  <a href="#link-como-contribuir">Como Contribuir</a>&nbsp;|&nbsp;';
     }
 
-    if(this.data.license) {
-      links += "\n  <a href=\"#licença\">Licença</a>";
+    if (this.data.license) {
+      links += '\n  <a href="#licença">Licença</a>';
     }
-        
+
     const headerLinks = `<h3 align="center">\n  ${links}\n</h3>`;
     this.headerLinks = headerLinks;
   },
   getImagesUrl() {
     this.images = "";
-    if(this.data.images && this.data.images.length > 0) {
+    if (this.data.images && this.data.images.length > 0) {
       for (const image of this.data.images) {
         this.images += `<img src="${image}" width="1200">\n`;
       }
     }
   },
   getImages() {
-    if(this.data.filesImages && this.data.filesImages.length > 0) {
+    if (this.data.filesImages && this.data.filesImages.length > 0) {
       for (const image of this.data.filesImages) {
-        if(image.fieldname === "logo_file") {
+        if (image.fieldname === "logo_file") {
           this.logo += `\n<p align="center">\n  <img src="${url}/uploads/${image.filename}" width="300" heigth="300">\n</p>`;
         }
-                
-        if(image.fieldname === "images_file") {
+
+        if (image.fieldname === "images_file") {
           this.images += `<img src="${url}/uploads/${image.filename}" width="1200">\n`;
         }
       }
     }
+  },
+  getRequirements() {
+    this.requirements = this.data.requirements;
   },
   getTecnologies() {
     this.tecnologies = "";
@@ -87,9 +98,10 @@ const Readme = {
   getContribution() {
     this.contribution = "";
     if (this.data.contribution == "sim") {
-      this.contribution = "- Faça um Fork do repositório\n- Clone o seu repositório\n- Crie uma branch com a sua feature\n- Faça um commit com suas mudanças\n- 'Push' a sua branch\n- Ir em Pull Requests do projeto original e criar uma pull request com o seu commit";
+      this.contribution =
+        "- Faça um Fork do repositório\n- Clone o seu repositório\n- Crie uma branch com a sua feature\n- Faça um commit com suas mudanças\n- 'Push' a sua branch\n- Ir em Pull Requests do projeto original e criar uma pull request com o seu commit";
     }
-  }
+  },
 };
 
 module.exports = Readme;
