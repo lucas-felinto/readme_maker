ec911efd98c50f446d379e82ecced1d58b095c22
Lucas Felinto <lucasfepinto@gmail.com>
2021-03-13 11:35:36 -0300

    feat: adding error deal

--- app/controllers/maker.js
+++ app/controllers/maker.js
@@ -3,37 +3,41 @@ let readme = null;
 
 module.exports = {
   post (req, res) {
-    const __1MegaByte = 1048576;
-    const maxOfImagesIncludingLogo = 5;
+    try {
+      const __1MegaByte = 1048576;
+      const maxOfImagesIncludingLogo = 5;
 
-    let filesImages = req.files.map((file, index) => {
-      if(index <= maxOfImagesIncludingLogo && file.size <= __1MegaByte) return file;
-    });
+      let filesImages = req.files.map((file, index) => {
+        if(index <= maxOfImagesIncludingLogo && file.size <= __1MegaByte) return file;
+      });
 
-    let features = req.body.features;
-    let images = req.body.images;
+      let features = req.body.features;
+      let images = req.body.images;
 
-    readme = {
-      ...req.body,
-      filesImages
-    };
-
-    if(features && images) {
-      features = features.filter(feature => feature);
-      images = images.filter(image => image);
       readme = {
         ...req.body,
-        filesImages,
-        features,
-        images
+        filesImages
       };
-    }
 
-    readme = createReadme.init(readme);
+      if (features && images) {
+        features = features.filter(feature => feature);
+        images = images.filter(image => image);
+        readme = {
+          ...req.body,
+          filesImages,
+          features,
+          images
+        };
+      }
+
+      readme = createReadme.init(readme);
         
-    return res.redirect("/ready");
+      return res.redirect("/ready");
+    } catch (e) {
+      return res.status(500).json({ error: "Internal Server Error."});
+    }
   },
   ready (req, res) {
-    return res.render("ready", {data: readme});
+    return res.render("ready", { data: readme });
   }
 };
